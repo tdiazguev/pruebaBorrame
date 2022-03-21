ad71b6be247d27ff74217b21bd2e1b04eed364c8
34676 <34676@LAPTOP-HMEPQPU1>
2022-03-21 15:48:06 +0100

    Hola mundo

---
+++ pruebaBorrame/src/pruebaborrame/PruebaBorrame.java
@@ -0,0 +1,14 @@
+
+package pruebaborrame;
+
+public class PruebaBorrame {
+
+   
+    public static void main(String[] args) {
+       
+        
+      System.out.println("Hola Mundo");
+        
+    }
+    
+}
